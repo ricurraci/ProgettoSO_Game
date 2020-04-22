1e1f997f62b5364d33d771333dddf65aa0f9df2f
ricurraci <riccardourraci@hotmail.it>
2020-04-23 00:57:05 +0200

    inizio gestione server

--- so_game_server.c
+++ so_game_server.c
@@ -4,6 +4,11 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <pthread.h>
 
 #include "image.h"
 #include "surface.h"
@@ -11,40 +16,147 @@
 #include "vehicle.h"
 #include "world_viewer.h"
 
+#define UDP_PORT        3000
+#define TCP_PORT        3000
+#define MAX_CONN_QUEUE  20
+#define UDP_BUFLEN      512
+
+
 int main(int argc, char **argv) {
-  if (argc<3) {
-    printf("usage: %s <elevation_image> <texture_image>\n", argv[1]);
-    exit(-1);
-  }
-  char* elevation_filename=argv[1];
-  char* texture_filename=argv[2];
-  char* vehicle_texture_filename="./images/arrow-right.ppm";
-  printf("loading elevation image from %s ... ", elevation_filename);
+  	if (argc<3) {
+    	printf("usage: %s <elevation_image> <texture_image>\n", argv[1]);
+    	exit(-1);
+  	}
+  	char* elevation_filename=argv[1];
+	char* texture_filename=argv[2];
+  	char* vehicle_texture_filename="./images/arrow-right.ppm";
+  	printf("loading elevation image from %s ... ", elevation_filename);
 
-  // load the images
-  Image* surface_elevation = Image_load(elevation_filename);
-  if (surface_elevation) {
-    printf("Done! \n");
-  } else {
-    printf("Fail! \n");
-  }
+  	// INIZIALIZZAZIONE UDP SERVER
+
+  	struct sockaddr_in serv_in = {0};
+  	int sock = socket(AF_INET, SOCK_DGRAM,0);
+
+  	memset((char *) serv_in, 0, sizeof(*serv_in));
+
+	serv_in->sin_family = AF_INET;
+	serv_in->sin_port = htons(UDP_PORT);
+	serv_in->sin_addr.s_addr = htonl(INADDR_ANY);
+
+	//binding
+	int res = bind(sock , (struct sockaddr*) serv_in, sizeof(*serv_in));
+	ERROR_HELPER(res, "Could not bind the udp_socket");
+
+	// FINE SETUP UDP
 
 
-  printf("loading texture image from %s ... ", texture_filename);
-  Image* surface_texture = Image_load(texture_filename);
-  if (surface_texture) {
-    printf("Done! \n");
-  } else {
-    printf("Fail! \n");
-  }
 
-  printf("loading vehicle texture (default) from %s ... ", vehicle_texture_filename);
-  Image* vehicle_texture = Image_load(vehicle_texture_filename);
-  if (vehicle_texture) {
-    printf("Done! \n");
-  } else {
-    printf("Fail! \n");
-  }
+	// INIZIALIZZAZIONE TCP SERVER
+
+	struct sockaddr_in server_addr = {0};
+	int sockaddr_len = sizeof(struct sockaddr_in);
+
+	// preparo la socket al listening
+	int socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+	ERROR_HELPER(socket_desc, "Could not create socket");
+
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(TCP_PORT);
+
+	// crash
+	int reuseaddr_opt = 1;
+
+	// operazioni per il socket 
+	int ret = setsockopt(socket_desc, SOL_SOCKET, SO_REUSEADDR, &reuseaddr_opt, sizeof(reuseaddr_opt));
+	ERROR_HELPER(ret, "Cannot set SO_REUSEADDR option");
+
+	// binding
+	ret = bind(socket_desc, (struct sockaddr *)&server_addr, sockaddr_len);
+	ERROR_HELPER(ret, "Cannot bind address to socket");
+
+	ret = listen(socket_desc, MAX_CONN_QUEUE);
+	ERROR_HELPER(ret, "Cannot listen on socket");
+
+	// FINE TCP SETUP
+
+
+
+
+	// load the images
+	Image* surface_elevation = Image_load(elevation_filename);
+	if (surface_elevation) {
+	  printf("Done! \n");
+	} else {
+	  printf("Fail! \n");
+	}
+
+  	printf("loading texture image from %s ... ", texture_filename);
+	Image* surface_texture = Image_load(texture_filename);
+	if (surface_texture) {
+	  printf("Done! \n");
+	} else {
+	  printf("Fail! \n");
+	}
+	
+	printf("loading vehicle texture (default) from %s ... ", vehicle_texture_filename);
+	Image* vehicle_texture = Image_load(vehicle_texture_filename);
+	if (vehicle_texture) {
+	  printf("Done! \n");
+	} else {
+	  printf("Fail! \n");
+	}
+
+
+	World_init(&world, surface_elevation, surface_texture, 0.5, 0.5, 0.5);
+	
+	is_running = 1;
+
+	// segnali da fare
+
+	List_init(&socket_list);
+
+	int id = 1;
+	int ret, client_desc;
+  
+	// creo i thread per udp
+	thread_args* udp_args = (thread_args*)malloc(sizeof(thread_args));
+	udp_args->socket_desc = udp_socket;
+	udp_args->id = -1;
+	
+	ret = pthread_create(&udp_thread, NULL, udp_handler, (void*)udp_args);
+	PTHREAD_ERROR_HELPER(ret, "Cannot create the udp_thread!");
+
+	int sockaddr_len = sizeof(struct sockaddr_in);
+
+	// alloco client_addr
+	struct sockaddr_in *client_addr = calloc(1, sizeof(struct sockaddr_in));
+
+
+	while(is_running) {
+
+		// accetto le connessioni dai client multipli
+		client_desc = accept(socket_desc, (struct sockaddr *)client_addr, (socklen_t *)&sockaddr_len);
+
+		// gestione errori
+		if (client_desc == -1 && errno == EINTR) continue; // check for interruption by signals
+		if(client_desc == -1 && run_server == 0) break;    // server is closing
+		ERROR_HELPER(client_desc, "Impossibile aprire la socket per le connessioni");
+
+		// gestione veicoli TODO		
+
+
+	}
+
+	// GESTIONE DELLA FINE todo
+
+
+
+
+
+
+
+
 
   // not needed here
   //   // construct the world
@@ -86,3 +198,4 @@ int main(int argc, char **argv) {
   // World_destroy(&world);
   return 0;             
 }
+
